3e90632560173d08e131e4692f5fb9daa50d9fe4
Dmitry Fedorov <dmitry_fedorov1@epam.com>
2023-02-22 13:33:18 +0300

    feat: is scope of task 3.2 create getProductsById

--- product-service/handler.js
+++ product-service/handler.js
@@ -8,3 +8,29 @@ module.exports.getProductsList = async () => {
     body: JSON.stringify(products),
   };
 };
+
+module.exports.getProductsById = async (event) => {
+  const querystring = event.queryStringParameters;
+  const productId = querystring?.productId ?? "";
+
+  if (!productId) {
+    return {
+      statusCode: 404,
+      body: "Not found",
+    };
+  }
+
+  const product = products.find((prod) => prod.id === productId);
+
+  if (product) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(product),
+    };
+  }
+
+  return {
+    statusCode: 404,
+    body: "Not found",
+  };
+};
